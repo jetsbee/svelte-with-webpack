b87d423cefac648040f72d6490e5af305444b8c2
Jaeseob Hwang <jetsbee@gmail.com>
2024-09-12 10:44:10 +0900

    Format

--- webpack.config.js
+++ webpack.config.js
@@ -6,63 +6,60 @@ const mode = process.env.NODE_ENV || 'development';
 const prod = mode === 'production';
 
 module.exports = {
-	entry: {
-		'build/bundle': ['./src/main.ts']
-	},
-	resolve: {
-		alias: {
-			svelte: path.resolve('node_modules', 'svelte') // Svelte 3
-		},
-		extensions: ['.mjs', '.js', '.ts', '.svelte'],
-		mainFields: ['svelte', 'browser', 'module', 'main'],
-		conditionNames: ['svelte', 'browser']
-	},
-	output: {
-		path: path.join(__dirname, '/public'),
-		filename: '[name].js',
-		chunkFilename: '[name].[id].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				loader: 'ts-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.svelte$/,
-				use: {
-					loader: 'svelte-loader',
-					options: {
-						compilerOptions: {
-							dev: !prod
-						},
-						emitCss: prod,
-						hotReload: !prod,
-						preprocess: sveltePreprocess({ sourceMap: !prod })
-					}
-				}
-			},
-			{
-				test: /\.css$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader'
-				]
-			}
-		]
-	},
-	mode,
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: '[name].css'
-		})
-	],
-	devtool: prod ? false : 'source-map',
-	devServer: {
-		hot: true,
-		static: {
-			directory: path.join(__dirname, 'public'),
-		}
-	}
+  entry: {
+    'build/bundle': ['./src/main.ts'],
+  },
+  resolve: {
+    alias: {
+      svelte: path.resolve('node_modules', 'svelte'), // Svelte 3
+    },
+    extensions: ['.mjs', '.js', '.ts', '.svelte'],
+    mainFields: ['svelte', 'browser', 'module', 'main'],
+    conditionNames: ['svelte', 'browser'],
+  },
+  output: {
+    path: path.join(__dirname, '/public'),
+    filename: '[name].js',
+    chunkFilename: '[name].[id].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.svelte$/,
+        use: {
+          loader: 'svelte-loader',
+          options: {
+            compilerOptions: {
+              dev: !prod,
+            },
+            emitCss: prod,
+            hotReload: !prod,
+            preprocess: sveltePreprocess({ sourceMap: !prod }),
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
+  mode,
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+  devtool: prod ? false : 'source-map',
+  devServer: {
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+  },
 };
